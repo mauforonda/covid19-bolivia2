04880011cbafea3e9d682b854ee40bb1e564e30a
Mauricio Foronda <josemauricioforonda@gmail.com>
2020-04-26 12:07:55 -0400

    init

---
+++ scripts/update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl -s -o 'nacional.csv' 'https://app.flourish.studio/api/data_table/2564902/csv'
+curl -s -o 'departamental.csv' 'https://app.flourish.studio/api/data_table/2770590/csv'
